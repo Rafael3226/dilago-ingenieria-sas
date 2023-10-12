0f5c4abf85f1555912a637816a3f5cbc45dd42c3
Rafael Uribe <53197581+Rafael3226@users.noreply.github.com>
2023-10-12 16:40:47 -0500

    Services pages stable

---
+++ src/components/pages/services/index.js
@@ -0,0 +1,17 @@
+import CathodicProtection from "./CathodicProtection.astro";
+import HydrostaticTesting from "./HydrostaticTesting.astro";
+import Inspections from "./Inspections.astro";
+import LoadAnalysis from "./LoadAnalysis.astro";
+import NonDestructiveTesting from "./NonDestructiveTesting.astro";
+import RiskAnalysis from "./RiskAnalysis.astro";
+import WeldingQuality from "./WeldingQuality.astro";
+
+export {
+  CathodicProtection,
+  HydrostaticTesting,
+  Inspections,
+  LoadAnalysis,
+  NonDestructiveTesting,
+  RiskAnalysis,
+  WeldingQuality,
+};
