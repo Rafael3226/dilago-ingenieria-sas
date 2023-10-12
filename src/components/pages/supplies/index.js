4c2da575ee0b8e0fc743465dfe2772d9dff29be6
Rafael Uribe <53197581+Rafael3226@users.noreply.github.com>
2023-10-12 14:15:02 -0500

    Update supplies imports

---
+++ src/components/pages/supplies/index.js
@@ -0,0 +1,5 @@
+import Electronic from "./Electronic.astro";
+import Mechanical from "./Mechanical.astro";
+import Suppliers from "./Suppliers.astro";
+
+export { Electronic, Mechanical, Suppliers };
