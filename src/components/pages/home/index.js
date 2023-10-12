4026b1c640c7abf31de6c42d7bd7045f2dd3ea50
Rafael Uribe <53197581+Rafael3226@users.noreply.github.com>
2023-10-12 11:44:08 -0500

    Sort components by page

---
+++ src/components/pages/home/index.js
@@ -0,0 +1,5 @@
+export * as HistorySection from "./HistorySection.astro";
+export * as BussinesSection from "./BussinesSection.astro";
+export * as ExperienceSection from "./ExperienceSection.astro";
+export * as ClientsSection from "./ClientsSection.astro";
+export * as ContactSection from "./ContactSection.astro";
