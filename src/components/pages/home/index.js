94bdb824b9e435132cfc9db88f84b4c05208b00f
Rafael Uribe <53197581+Rafael3226@users.noreply.github.com>
2023-10-12 13:56:50 -0500

    Supplies page stable

--- src/components/pages/home/index.js
+++
@@ -1,5 +0,0 @@
-export * as HistorySection from "./HistorySection.astro";
-export * as BussinesSection from "./BussinesSection.astro";
-export * as ExperienceSection from "./ExperienceSection.astro";
-export * as ClientsSection from "./ClientsSection.astro";
-export * as ContactSection from "./ContactSection.astro";
