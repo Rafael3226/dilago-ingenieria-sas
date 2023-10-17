c07220aa3250c1c7254565eb11484f91ab20380b
Rafael Uribe <53197581+Rafael3226@users.noreply.github.com>
2023-10-17 16:12:53 -0500

    Text XL update, Single source of truth for brochures

---
+++ src/constants/brochures.js
@@ -0,0 +1,16 @@
+import TrainingPDF from "../assets/pdf/brochure_capacitaciones.pdf";
+import CorporativePDF from "../assets/pdf/Presentación corporativa.pdf";
+
+const title = "Descargar Brochure";
+
+export const training = {
+  name: "Brochure Capacitaciones.pdf",
+  title,
+  href: TrainingPDF,
+};
+
+export const corporative = {
+  name: "Dilago Brochure.pdf",
+  title,
+  href: CorporativePDF,
+};
