2460fed53d04dc6139c9be65375a5e8f8b903d40
Rafael Uribe <53197581+Rafael3226@users.noreply.github.com>
2023-10-20 16:14:45 -0500

    Remove inspections section

--- src/components/pages/services/index.js
+++ src/components/pages/services/index.js
@@ -1,6 +1,5 @@
 import CathodicProtection from "./CathodicProtection.astro";
 import HydrostaticTesting from "./HydrostaticTesting.astro";
-import Inspections from "./Inspections.astro";
 import NonDestructiveTesting from "./NonDestructiveTesting.astro";
 import RiskAnalysis from "./RiskAnalysis.astro";
 import WeldingQuality from "./WeldingQuality.astro";
@@ -9,7 +8,6 @@ import TechnicalConsulting from "./TechnicalConsulting.astro";
 export {
   CathodicProtection,
   HydrostaticTesting,
-  Inspections,
   NonDestructiveTesting,
   RiskAnalysis,
   WeldingQuality,
