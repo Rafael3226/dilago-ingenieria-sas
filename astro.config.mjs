12cee6cad5229b522e1b437bbbeb9e7354955051
Rafael Uribe. <Rafael.Uribe.2080@gmail.com>
2023-11-29 14:40:16 -0500

    Add Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from "astro/config";
-
+import tailwind from "@astrojs/tailwind";
 const PORT = import.meta.env.PORT || 3000;
+
 // https://astro.build/config
 export default defineConfig({
-  server: { port: PORT }
+  server: {
+    port: PORT
+  },
+  integrations: [tailwind()]
 });
