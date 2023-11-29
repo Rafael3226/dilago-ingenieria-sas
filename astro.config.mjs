e5dc355cc1e56d47d37c545c759d5c23cb1c3145
Rafael Uribe. <Rafael.Uribe.2080@gmail.com>
2023-11-29 18:00:26 -0500

    Remove unused packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 const PORT = import.meta.env.PORT || 3000;
 
 // https://astro.build/config
 export default defineConfig({
   server: {
-    port: PORT
+    port: PORT,
   },
-  integrations: [tailwind()]
 });
