c757fe65f1a97696a75fb33b06ff7dc0838cfa56
Mottaaa26 <molinamottap@gmail.com>
2026-03-02 21:45:09 -0500

    Reemplazar portafolio por versión comprimida

--- src/constants/brochures.js
+++ src/constants/brochures.js
@@ -1,6 +1,6 @@
 import TrainingPDF from "../assets/pdf/brochure_capacitaciones.pdf";
 import ServicesPDF from "../assets/pdf/brochure_servicios.pdf";
-import Portfolio2026 from "../assets/pdf/Portafolio_dilago_2026.pdf";
+import Portfolio2026 from "../assets/pdf/Portafolio_dilago_2026-comprimido.pdf";
 
 const title = "Descargar Brochure";
 
